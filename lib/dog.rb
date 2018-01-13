295d5b06477deee1c6676caa5f1d517903a8f5ca
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-01-13 04:05:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,10 @@ class Dog
     @name = name
   end
 
-  def name=(name)
-    @name = name
-  end
-  def name
-    @name
-  end
+#  def name=(name)
+#    @name = name
+#  end
+#  def name
+#    @name
+#  end
 end
-
-
