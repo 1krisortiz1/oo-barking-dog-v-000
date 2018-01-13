edfb87fcfa418bcc0d14fce4a49e93512c511784
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-01-13 04:04:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,17 @@
 # Your code goes here!
 class Dog
+  def initialize(name)
+    @name = name
+  end
 
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
 end
+
+fido = Dog.new("Fido")
+
+fido.name
